606f5662534462d98da9e9c72e691e5e017e2641
MKen212 <46496040+MKen212@users.noreply.github.com>
2021-02-20 12:56:17 +0100

    First Commit

---
+++ welcome.js
@@ -0,0 +1,7 @@
+let todaysDate = Date();
+
+let total = 2 + 2;
+
+let message = "Hello User. Today's date is: " + todaysDate + ". The answer to 2 + 2 = " + total;
+
+document.getElementById("welcome").innerHTML = message;
